fb9b9722ae017156b6966162778c7c25e0cb9cb1
vdragon1225 <23laidan@gmail.com>
2026-04-09 19:26:13 -0500

    add more common skills

--- src/common_skills.py
+++ src/common_skills.py
@@ -3,27 +3,49 @@ common_skills = [
     # Programming / Tech
     'python', 'java', 'c++', 'sql', 'c#',
     'r', 'javascript', 'html', 'css',
+    'pyspark', 'spark', 'hadoop', 'nosql',
 
     # Frameworks / Libraries
     'react', 'node.js', 'tensorflow',
-    'pandas', 'numpy',
+    'pandas', 'numpy', 'scikit-learn',
 
     # Data & ML
     'data analysis', 'data analytics',
     'data science', 'data visualization',
     'statistical analysis', 'big data',
     'exploratory data analysis', 'data mining',
+    'machine learning', 'forecasting',
+    'cohort analysis', 'funnel analysis',
+    'hypothesis testing', 'churn analysis',
+    'kpi development', 'a/b testing',
+    'experimentation',
+
+    # Data Engineering / Warehousing
+    'etl', 'elt', 'dbt', 'airflow',
+    'snowflake', 'bigquery', 'redshift',
+    'databricks',
 
     # Tools / Platforms
     'excel', 'tableau', 'power bi',
+    'looker', 'looker studio', 'qlik',
+    'google analytics', 'mixpanel', 'amplitude',
     'git', 'github', 'jira', 'confluence',
     'docker', 'kubernetes',
-    'aws', 'azure', 'linux', 'windows', 'macos',
+    'aws', 'azure', 'gcp',
+    'aws s3', 'ec2', 'lambda', 'azure synapse',
+    'linux', 'windows', 'macos',
+
+    # Databases
+    'postgresql', 'mysql', 'sql server',
 
     # Business / Reporting
     'reporting', 'business analysis',
     'market research', 'financial analysis',
     'project management', 'agile methodologies',
+    'stakeholder management', 'requirements gathering',
+    'storytelling', 'dashboard design',
+    'cross-functional collaboration',
+    'prompt engineering',
 
     # Soft Skills
     'analytical skills', 'problem-solving skills',
